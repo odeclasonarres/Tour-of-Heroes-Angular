16cd078cad58150fe3df96a1d7c7a11cbf794f57
odeclasonarres <odeclasonarres@gmail.com>
2019-04-02 13:57:33 +0200

    medio

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,8 +10,7 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
@@ -23,4 +22,13 @@ export class HeroService {
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
+
+    /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+
 }
